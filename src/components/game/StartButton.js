253e1854a7e78d2dd8f44ab5dbe3dceee97a23ae
Johann <johanson88@gmail.com>
2020-07-09 12:28:21 +0200

    Add StartButton component

---
+++ src/components/game/StartButton.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const StartButton = () => <button>Start!</button>
+
+export default StartButton;
--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+
+//Components
+import StartButton from './../components/game/StartButton';
 
 const Game = () => {
+    const { gameMode } = useParams();
     return(
-        <h1>Game</h1>
+        <div>
+            <h1>Game</h1>
+            <h2>{ gameMode }</h2>
+            <StartButton />
+        </div>
     )
 }
 
